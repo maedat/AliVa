2a82b68df47716fcd39b8a5181580f1cde0af31f
Taro Maeda <maedat@nibb.ac.jp>
2019-09-29 18:44:44 +0900

    v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-AliVa
+
+# AliVa; Simple nucleaotide variation counter
+====
+
+AliVa is a simple counter and visualizar of aligned nucleotide sequence variation. AliVa counts the  indel/transition/transversion numbers from an multiple aligned nulceotide sequence file, and visularize the distribution of the variation with multiple plots. Our main purpose is the understanding of variation-enriched region on the aligned sequences. We split the alignemt with given window size (dafault = 50 bp), and caluclate the number of variation in each window. The variations will be counted via round robin among all aligned sequences and raw data will output to "result_rawdata.txt" file, and the mean values will described on "average.txt" file. The main output plot is "line_all.pdf" which visualize the distribution of the variations. 
+
+## Requirement
+
+Python 3.7. (Biopython, pandas, argparse, matplotlib, seaborn, numpy)
+
+## Usage
+```sh
+usage: aliva.py [-h] -f FASTA [-w WIN] [-p]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f FASTA, --fasta FASTA
+                        File path to a genome sequence file
+  -w WIN, --win WIN     window size (bp) (default = 50)
+  -p, --plot            Manualy modify the plot with seaborn window
+
+
+```
+
+## Demo
+```sh
+
+aliva.py -f ./test/test.fa 
+
+```
+
+
+## Licence
+[MIT License](http://opensource.org/licenses/mit-license.php)
+
+## Author
+[Taro Maeda](https://github.com/maedat)
--- aliva.py
+++ aliva.py
@@ -5,7 +5,7 @@
 # ======================================================================
 # Project Name    : AliVa
 # File Name       : aliva.py
-# Version       : 0.0.1
+# Version       : 1.0.0
 # Encoding        : python
 # Creation Date   : 2019/09/1
 # Author : Taro Maeda 
@@ -20,17 +20,20 @@ import argparse
 import pandas as pd
 from Bio import SeqIO
 from Bio import Seq
-from BCBio import GFF
 
-
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set() #seabornライブラリを読み込み、スタイルをセットする
+import numpy as np
+import pandas as pd
 
 
 
 def GET_ARGS():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f','--fasta',  help="File path to a genome sequence file", required=True)
-    parser.add_argument('-k','--kmer', help="k-mear number", required=False)
-    parser.set_defaults(kmer=50)
+    parser.add_argument('-w','--win', help="window size (bp) (default = 50)", required=False)
+    parser.add_argument('-p','--plot', help="Manualy modify the plot with seaborn window", action='store_true', required=False)
+    parser.set_defaults(win=50)
     return parser.parse_args()
 
 
@@ -61,14 +64,21 @@ def VAL_COUNTER(F1_seq_CHA, F2_seq_CHA):
                     
 
 
-
+nCr = {}
+def cmb(n, r):
+    if r == 0 or r == n: return 1
+    if r == 1: return n
+    if (n,r) in nCr: return nCr[(n,r)]
+    nCr[(n,r)] = cmb(n-1,r) + cmb(n-1,r-1)
+    return nCr[(n,r)]
 
 
 
 if __name__ == '__main__':
     args = GET_ARGS()
     fasta_in = args.fasta
-    k_num = args.kmer
+    k_num = args.win
+    plot_flag = args.plot
     
     
     MUCH_num = 0
@@ -76,7 +86,14 @@ if __name__ == '__main__':
     Indel =0 
     Transver =0
     Transition =0
+    Already_done =[]
 
+    label = ["Pair", "start", "end", "Consensus", "Indel", "Transversion", "Transition"]
+    labels = '\t'.join(label)
+    
+    
+    df = pd.DataFrame(columns=label)
+    
     entries = [(fasta_in, fasta_in)]
     for index, (fasta_1, fasta_2) in enumerate(entries):
     ##fastaから配列を順番に読み込む
@@ -93,19 +110,112 @@ if __name__ == '__main__':
                         F2_seq = rec2.seq.upper() #配列を大文字で取得する
                         if F2_id != F1_id:
                             Pair = F1_id +  "vs" + F2_id
-                            for cut_length in range(0, length - k_num):
-                                F1_cut = F1_seq[cut_length:cut_length+k_num]
-                                F2_cut = F2_seq[cut_length:cut_length+k_num]
-                                for seq_num in range(k_num):
-                                    tmp_MUCH_num, tmp_Indel, tmp_Transver, tmp_Transition  = VAL_COUNTER(F1_cut[seq_num], F2_cut[seq_num])
-                                    MUCH_num += tmp_MUCH_num
-                                    Indel    += tmp_Indel
-                                    Transver += tmp_Transver
-                                    Transition += tmp_Transition
-                                OUT = [Pair, str(cut_length), str(cut_length+k_num), str(MUCH_num), str(Indel), str(Transver), str(Transition)]
-                                OUTs = '\t'.join(OUT)
-                                print(OUTs)
-                                MUCH_num = 0
-                                Indel =0 
-                                Transver =0
-                                Transition =0
+                            if Pair in Already_done:
+                                for cut_length in range(0, length - k_num):
+                                    F1_cut = F1_seq[cut_length:cut_length+k_num]
+                                    F2_cut = F2_seq[cut_length:cut_length+k_num]
+                                    for seq_num in range(k_num):
+                                        tmp_MUCH_num, tmp_Indel, tmp_Transver, tmp_Transition  = VAL_COUNTER(F1_cut[seq_num], F2_cut[seq_num])
+                                        MUCH_num += tmp_MUCH_num
+                                        Indel    += tmp_Indel
+                                        Transver += tmp_Transver
+                                        Transition += tmp_Transition
+                                        cut_length_mod = cut_length+k_num
+                                    sdf= pd.DataFrame(
+                                                        {'Pair': Pair,
+                                                        'start': cut_length,
+                                                        'end': cut_length_mod,
+                                                        'Consensus': MUCH_num,
+                                                        'Indel': Indel,
+                                                        'Transversion': Transver,
+                                                        'Transition': Transition}, index= [Pair + str(cut_length)] )
+                                    df = df.append(sdf)
+                                    MUCH_num = 0
+                                    Indel =0 
+                                    Transver =0
+                                    Transition =0
+                            Already_done.append(F2_id+  "vs" + F1_id)
+                            
+                            
+                            
+
+                                
+    sns.set_style('whitegrid')
+    
+    df.start = df.start.astype(int)    
+    df.end = df.end.astype(int)    
+    df.Transversion = df.Transversion.astype(float)    
+    df.Transition = df.Transition.astype(float)    
+    df.Indel = df.Indel.astype(float)    
+    df.Consensus = df.Consensus.astype(float)  
+    
+    df_mod =df[['start', 'Consensus','Indel','Transversion', 'Transition']]
+    grouped = df_mod.groupby(['start'])
+    groupde_mean = grouped.mean()
+    
+    groupde_mean.to_csv("average.txt", sep="\t")
+    df.to_csv("result_rawdata.txt", sep="\t")
+
+    
+    
+    plt.figure()
+    sns.countplot(x="Transition", data=df)
+    plt.xlabel('Number of Transition par window')
+    plt.savefig("hist_Transition.pdf")      
+    if plot_flag == True:
+        plt.show()
+    
+    plt.figure()
+    sns.countplot(x="Transversion", data=df)
+    plt.xlabel('Number of Transversion par window')
+    plt.savefig("hist_Transversion.pdf")      
+    if plot_flag == True:
+        plt.show()    
+    
+    plt.figure()
+    sns.countplot(x="Indel", data=df)
+    plt.xlabel('Number of Indel par window')
+    plt.savefig("hist_Indel.pdf")      
+    if plot_flag == True:
+        plt.show()
+
+    plt.figure()
+    g = sns.JointGrid(x="Transition", y ="Transversion", data=df)
+    g.plot(sns.regplot, sns.distplot)
+    plt.savefig("Transversion_vs_Transversion.pdf")
+    plt.xlim(0,k_num)
+    plt.ylim(0,k_num)
+    if plot_flag == True:
+        plt.show()
+
+    plt.figure()
+    sns.lineplot(x="start", y ="Transversion", hue = "Pair", data=df)
+    plt.xlabel('start position of window')
+    plt.savefig("line_Transition.pdf")      
+    if plot_flag == True:
+        plt.show()    
+    
+    plt.figure()
+    sns.lineplot(x="start", y ="Transition", hue = "Pair", data=df)
+    plt.xlabel('start position of window')
+    plt.savefig("line_Transition.pdf")      
+    if plot_flag == True:
+        plt.show()    
+    
+    plt.figure()
+    sns.lineplot(x="start", y ="Indel", hue = "Pair", data=df)
+    plt.xlabel('start position of window')
+    plt.savefig("line_indel.pdf")      
+    if plot_flag == True:
+        plt.show()
+    
+    df_mod = df.melt(var_name = "mutation type", value_name = "mutation par window", id_vars = ["Pair", "start", "end"])
+
+    
+    plt.figure()
+    sns.lineplot(x="start", y ="mutation par window", hue="mutation type", data=df_mod, ci="sd")
+    plt.xlabel('start position of window')
+    plt.ylabel('mutation par window (Mesh = SD)')
+    plt.savefig("line_all.pdf")      
+    if plot_flag == True:
+        plt.show()
